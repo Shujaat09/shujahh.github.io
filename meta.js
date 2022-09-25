de8cd9552f1cf661c995cacc8f68f28313838b44
Shujaat09 <112425715+Shujaat09@users.noreply.github.com>
2022-09-25 21:34:55 +0530

    Delete meta.js

--- meta.js
+++
@@ -1,5 +0,0 @@
-var pageContent = document.getElementById("content"),
-	pagecopy = pageContent.cloneNode(true),
-	blurryContent = document.getElementById("blurryscroll");
-	blurryContent.appendChild(pagecopy);
-	window.onscroll = function() { blurryContent.scrollTop = window.pageYOffset; }
